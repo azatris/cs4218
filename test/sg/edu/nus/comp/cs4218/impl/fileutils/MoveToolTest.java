0d62124457c4accd4f3f1aa2597672247128b30d
Mingxia Liu <liimx@yahoo.com>
2014-02-04 17:12:32 +0800

    java library import

--- test/sg/edu/nus/comp/cs4218/impl/fileutils/MoveToolTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/fileutils/MoveToolTest.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Random;
 
 import org.junit.After;
