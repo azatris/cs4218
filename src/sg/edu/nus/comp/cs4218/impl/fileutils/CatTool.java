0e5916b4ab53accb005f758416c2a1dc6107f35d
Mingxia Liu <liimx@yahoo.com>
2014-02-06 23:00:28 +0800

    Add args == null to CatTool and MoveTool. All test cases down for EchoToolTest

--- src/sg/edu/nus/comp/cs4218/impl/fileutils/CatTool.java
+++ src/sg/edu/nus/comp/cs4218/impl/fileutils/CatTool.java
@@ -15,7 +15,7 @@ public class CatTool extends ATool implements ICatTool {
 
 	public CatTool(String[] arguments) {
 		super(arguments);
-		if (args.length == 0 || !args[0].equals("cat")) {
+		if (args == null ||args.length == 0 || !args[0].equals("cat")) {
 			setStatusCode(127);
 			
 		}
--- src/sg/edu/nus/comp/cs4218/impl/fileutils/MoveTool.java
+++ src/sg/edu/nus/comp/cs4218/impl/fileutils/MoveTool.java
@@ -15,7 +15,7 @@ public class MoveTool extends ATool implements IMoveTool {
 
 	public MoveTool(String[] arguments) {
 		super(arguments);
-		if (args.length == 0 || !args[0].equals("move")) {
+		if (args == null || args.length == 0 || !args[0].equals("move")) {
 			setStatusCode(127);
 		}
 	}
