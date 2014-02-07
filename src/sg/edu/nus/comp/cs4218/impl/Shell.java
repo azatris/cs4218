8367ab659ebcbe72e2b87029bdbb98bf2fa77896
Andrianto Lie <andrianto.lie@gmail.com>
2014-02-07 10:39:42 +0800

    - add javadoc to Shell.java - add private testing for ExecutingCommandThread - testing Shell.java

--- src/sg/edu/nus/comp/cs4218/impl/Shell.java
+++ src/sg/edu/nus/comp/cs4218/impl/Shell.java
@@ -23,171 +23,171 @@ import sg.edu.nus.comp.cs4218.impl.fileutils.WrongParsingTool;
  */
 public class Shell implements IShell {
 
-		private static final int ONE=1;
-        private static final int TWO=2;
-        private static final int THREE=3;
-        File workingDirectory = new File(System.getProperty("user.dir"));
-        private static PrintStream out = System.out;
-	
-	@Override
-	public ITool parse(final String commandline) {
-                ITool tool;
-                final String[] pipe=commandline.split(" \\| ");
-                if(pipe.length>ONE){
-                		ITool[] allTools = parseWithPipe(pipe);
-                        tool = new PipingTool(allTools);
-                }
-                else if(pipe.length==ONE){
-                        String[] commandArray;
-                        String argument = pipe[0];
-                        argument = argument.trim();
-                        commandArray = getCommandArray(argument);
-                        tool = getWhatTool(commandArray);
-                } else {
-                        tool = null;
-                }
-                return tool;
-        }      
-       
-        /**
-         * Method of creating the PipingTool
-         * @param pipe String
-         * @return A PipingTool of the type ITool.
-         */
-        public ITool[] parseWithPipe(String[] pipe){
-                ITool[] work = new ITool[pipe.length];
-                String[] commandArray;
-                for(int i=0; i<pipe.length; i++){
-                        commandArray = getCommandArray(pipe[i]);
-                        work[i]= getWhatTool(commandArray);
-                }
-                return work;
-        }
- 
-        /**
-         *  
-         * @param argument
-         * @return
-         */
-        public String[] getCommandArray(final String argument){
-                // Something could go wrong
-                final String[] hasPattern = argument.split(" \"|\" ");// . ". or ." .
-                String[] argumentArray;
-                if(hasPattern.length == THREE && argument.startsWith("grep")){
-                        argumentArray = patternForGrep(hasPattern);
-                }
-                else if(hasPattern.length == ONE){
-                        argumentArray = argument.split(" ");
-                }
-                else if(hasPattern.length != ONE || hasPattern.length != THREE){
-                        argumentArray = new String[ONE];
-                        argumentArray[0] = "Parsing failed"; //TODO
- 
-                }
-                else{
-                        argumentArray = new String[ONE];
-                        argumentArray[0] = "Parsing failed"; //TODO
- 
-                }
-                return argumentArray;
-        }
-       
-        /**
-         * The parser if a pattern and grep is the next running iTool
-         * @param hasPattern A string array of length 3 with command and maybe option it there is any
-         * as first parameter, The pattern as is second and the filename as the third paramenter.
-         * @return A argument array ready to process by grep.
-         */
-        public String[] patternForGrep(final String[] hasPattern){
-                final String commandAndOption = hasPattern[0];
-                final String pattern=hasPattern[ONE];
-                final String file = hasPattern[TWO];
-                final String[] firstPart = commandAndOption.split(" ");
-                final String[] files = file.split(" ");
-                String[] argumentArray = new String[firstPart.length + ONE + files.length];
-                int loopVarible;
-                for(loopVarible = 0; loopVarible<firstPart.length; loopVarible++){
-                        argumentArray[loopVarible]=firstPart[loopVarible];
-                }
-                argumentArray[loopVarible] = pattern;
-                loopVarible++;
-                for(int i=0; loopVarible<argumentArray.length ; loopVarible++, i++){
-                        argumentArray[loopVarible] = files[i];
-                }
- 
-                return argumentArray;
-        }
- 
- 
- 
-        /**
-         *
-         * @param arguments
-         * @param tool
-         * @return The new Command that should be ececuted
-         */
-        private ITool getWhatTool(final String[] arguments){
-                ITool newCommand;
-                String tool = arguments[0];
-                switch(tool){
-                case "grep":
-                        newCommand = new GrepTool(arguments);
-                        break;
-                case  "cat":
-                        newCommand = new CatTool(arguments);
-                        break;
-                case "cd":
-                        newCommand = new CdTool(arguments);
-                        break;
-                case "copy":
-                        newCommand =  new CopyTool(arguments);
-                        break;
-                case "delete":
-                        newCommand = new DeleteTool(arguments);
-                        break;
-                case "echo":
-                        newCommand = new EchoTool(arguments);
-                        break;
-                case "ls":
-                        newCommand = new LsTool(arguments);
-                        break;
-                case "move":
-                        newCommand = new MoveTool(arguments);
-                        break;
-                case "pwd":
-                        newCommand = new PWDTool(arguments);
-                        break;
-                case "Parsing failed":
-                        newCommand = new WrongParsingTool(arguments);
-	                    System.out.println("Wrong parsing");
-                default:
-                        newCommand = null;
-                }
-                return newCommand;
-        }
+	private static final int ONE=1;
+	private static final int TWO=2;
+	private static final int THREE=3;
+	File workingDirectory = new File(System.getProperty("user.dir"));
+	private static PrintStream out = System.out;
 
 	@Override
-        public Runnable execute(ITool tool) {
-                // TODO Implement
-                String stdin = null;
-                final Thread runningThread = new ExecutingCommandThread(tool, stdin);
-                runningThread.start();
-                return runningThread;
-        }
- 
-        @Override
-        public void stop(Runnable toolExecution) {
-                //TODO Implement
-        		final Thread runningThread = (Thread)toolExecution;
-        		if (runningThread != null){
-        			if(runningThread.isAlive()){
-        				runningThread.interrupt();
-        			}
-        		}
-        		else{
-        			out.println("No Running Thread!");
-        		}
-        }
+	public ITool parse(final String commandline) {
+		ITool tool;
+		final String[] pipe=commandline.split(" \\| ");
+		if(pipe.length>ONE){
+			ITool[] allTools = parseWithPipe(pipe);
+			tool = new PipingTool(allTools);
+		}
+		else if(pipe.length==ONE){
+			String[] commandArray;
+			String argument = pipe[0];
+			argument = argument.trim();
+			commandArray = getCommandArray(argument);
+			tool = getWhatTool(commandArray);
+		} else {
+			tool = null;
+		}
+		return tool;
+	}      
+
+	/**
+	 * Method of creating the PipingTool
+	 * @param pipe String
+	 * @return A PipingTool of the type ITool.
+	 */
+	public ITool[] parseWithPipe(String[] pipe){
+		ITool[] work = new ITool[pipe.length];
+		String[] commandArray;
+		for(int i=0; i<pipe.length; i++){
+			commandArray = getCommandArray(pipe[i]);
+			work[i]= getWhatTool(commandArray);
+		}
+		return work;
+	}
+
+	/**
+	 *  
+	 * @param argument
+	 * @return
+	 */
+	public String[] getCommandArray(final String argument){
+		// Something could go wrong
+		final String[] hasPattern = argument.split(" \"|\" ");// . ". or ." .
+		String[] argumentArray;
+		if(hasPattern.length == THREE && argument.startsWith("grep")){
+			argumentArray = patternForGrep(hasPattern);
+		}
+		else if(hasPattern.length == ONE){
+			argumentArray = argument.split(" ");
+		}
+		else if(hasPattern.length != ONE || hasPattern.length != THREE){
+			argumentArray = new String[ONE];
+			argumentArray[0] = "Parsing failed"; //TODO
+
+		}
+		else{
+			argumentArray = new String[ONE];
+			argumentArray[0] = "Parsing failed"; //TODO
+
+		}
+		return argumentArray;
+	}
+
+	/**
+	 * The parser if a pattern and grep is the next running iTool
+	 * @param hasPattern A string array of length 3 with command and maybe option it there is any
+	 * as first parameter, The pattern as is second and the filename as the third paramenter.
+	 * @return A argument array ready to process by grep.
+	 */
+	public String[] patternForGrep(final String[] hasPattern){
+		final String commandAndOption = hasPattern[0];
+		final String pattern=hasPattern[ONE];
+		final String file = hasPattern[TWO];
+		final String[] firstPart = commandAndOption.split(" ");
+		final String[] files = file.split(" ");
+		String[] argumentArray = new String[firstPart.length + ONE + files.length];
+		int loopVarible;
+		for(loopVarible = 0; loopVarible<firstPart.length; loopVarible++){
+			argumentArray[loopVarible]=firstPart[loopVarible];
+		}
+		argumentArray[loopVarible] = pattern;
+		loopVarible++;
+		for(int i=0; loopVarible<argumentArray.length ; loopVarible++, i++){
+			argumentArray[loopVarible] = files[i];
+		}
+
+		return argumentArray;
+	}
+
+
+
+	/**
+	 *
+	 * @param arguments
+	 * @param tool
+	 * @return The new Command that should be ececuted
+	 */
+	private ITool getWhatTool(final String[] arguments){
+		ITool newCommand;
+		String tool = arguments[0];
+		switch(tool){
+		case "grep":
+			newCommand = new GrepTool(arguments);
+			break;
+		case  "cat":
+			newCommand = new CatTool(arguments);
+			break;
+		case "cd":
+			newCommand = new CdTool(arguments);
+			break;
+		case "copy":
+			newCommand =  new CopyTool(arguments);
+			break;
+		case "delete":
+			newCommand = new DeleteTool(arguments);
+			break;
+		case "echo":
+			newCommand = new EchoTool(arguments);
+			break;
+		case "ls":
+			newCommand = new LsTool(arguments);
+			break;
+		case "move":
+			newCommand = new MoveTool(arguments);
+			break;
+		case "pwd":
+			newCommand = new PWDTool(arguments);
+			break;
+		case "Parsing failed":
+			newCommand = new WrongParsingTool(arguments);
+			System.out.println("Wrong parsing");
+		default:
+			newCommand = null;
+		}
+		return newCommand;
+	}
+
+	@Override
+	public Runnable execute(ITool tool) {
+		// TODO Implement
+		String stdin = null;
+		final Thread runningThread = new ExecutingCommandThread(tool, stdin);
+		runningThread.start();
+		return runningThread;
+	}
+
+	@Override
+	public void stop(Runnable toolExecution) {
+		//TODO Implement
+		final Thread runningThread = (Thread)toolExecution;
+		if (runningThread != null){
+			if(runningThread.isAlive()){
+				runningThread.interrupt();
+			}
+		}
+		else{
+			out.println("No Running Thread!");
+		}
+	}
 
 	/**
 	 * Do Forever
@@ -199,99 +199,121 @@ public class Shell implements IShell {
 	 * 6. Report the exit status of the command to the user
 	 */
 	public static void main(final String[] args){
-                //TODO Implement
-                final Shell shell = new Shell(); //constructor
-                out.print("Welcome to CS4218 Shell\n");
- 
-                String commandLine;
-                ITool parseResult;
-                final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
- 
-                Thread executingThread = null; //Thread responsible for executing the command
- 
-                do{
-                        try {
-                                commandLine = input.readLine(); //Get the command line
-                                if(commandLine.equalsIgnoreCase("ctrl-z")||
-                                		commandLine.equalsIgnoreCase("ctrl z")||
-                                		commandLine.equalsIgnoreCase("ctrlz")){
-                                		out.println("Stopping the running thread");
-                                        shell.stop(executingThread); //stop the execution when it reads ctrl-z
-                                }
-                                else{
-                                        parseResult = shell.parse(commandLine); //return the result of parsing
-                                        executingThread = (Thread)shell.execute(parseResult); //run the thread
-                                }
-                        } catch (IOException e) {
-                                // TODO Auto-generated catch block
-                                e.printStackTrace();
-                        }
-                }
-                while(true);
- 
-        }
-        
- 
-        public class ExecutingCommandThread extends Thread{
- 
-                private ITool executionTool;
-                private String standardIn;
-                public ExecutingCommandThread(final ITool tool, final String stdin){
-                		super();
-                        executionTool = tool;
-                        standardIn = stdin;
-                }
-                
-                //Get Error Message based on Status Code (http://tldp.org/LDP/abs/html/exitcodes.html#EXITCODESREF)
-                private String getMessage(final int statusCode, final String executionResult){
-                	String message = null;
-                	switch(statusCode){
-                	case 0:
-                		message = executionResult;
-                		break;
-                	case 1:
-                		message = "General Error Detected";
-                		break;
-                	case 2:
-                		message = "Missing Keyword or Command";
-                		break;
-                	case 55:
-                		//55 is our team special defined code to notify the shell to change workingDirectory
-                		workingDirectory = new File(executionResult);
-                		message = executionResult;
-                		break;
-                	case 98:
-                		message = "Parsing Failed";
-                		break;
-                	case 126:
-                		message = "Command invoked cannot execute";
-                		break;
-                	case 127:
-                		message = "Command not found";
-                		break;
-                	case 210:
-                		message = "A command was null";
-                	default:
-                		message = "Error Detected";
-                		break;
-                	}
-                	return message;              	
-                }
-        
-                @Override
-                public void run() {
-                        // TODO Auto-generated method stub
-                        if(executionTool != null){
-                                final String executionResult = executionTool.execute(workingDirectory, standardIn);
-                                if(executionResult != null){
-                                	String message = getMessage(executionTool.getStatusCode(), executionResult);
-                                    out.println(message);            
-                                }
-                        }
-                        else{
-                        	out.println("There is no tool associated with the given command!");
-                        }
-                }
-        }
-               
+		//TODO Implement
+		final Shell shell = new Shell(); //constructor
+		out.print("Welcome to CS4218 Shell\n");
+
+		String commandLine;
+		ITool parseResult;
+		final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+		Thread executingThread = null; //Thread responsible for executing the command
+
+		do{
+			try {
+				commandLine = input.readLine(); //Get the command line
+				if(commandLine.equalsIgnoreCase("ctrl-z")||
+						commandLine.equalsIgnoreCase("ctrl z")||
+						commandLine.equalsIgnoreCase("ctrlz")){
+					out.println("Stopping the running thread");
+					shell.stop(executingThread); //stop the execution when it reads ctrl-z
+				}
+				else{
+					parseResult = shell.parse(commandLine); //return the result of parsing
+					executingThread = (Thread)shell.execute(parseResult); //run the thread
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		while(true);
+
+	}
+
+	/**
+	 * Thread for handling and executing ITool
+	 */
+	public class ExecutingCommandThread extends Thread{
+
+		private ITool executionTool = null;
+		private String standardIn = null;
+		
+		/**
+         * Constructor, instantiate tool and standard in
+         * @param tool that we want to run and standard in
+         */
+		public ExecutingCommandThread(final ITool tool, final String stdin){
+			super();
+			executionTool = tool;
+			standardIn = stdin;
+		}
+		
+        /**
+         * Method for getting a particular Message based on the status code (http://tldp.org/LDP/abs/html/exitcodes.html#EXITCODESREF)
+         * User defined status code = {55: change working directory, 98: error in parsing}
+         * @return a message to be printed on the shell
+         */
+		public String getMessage(final int statusCode, final String executionResult){
+			String message = null;
+			switch(statusCode){
+			case 0:
+				message = executionResult;
+				break;
+			case 1:
+				message = "General Error Detected";
+				break;
+			case 2:
+				message = "Missing Keyword or Command";
+				break;
+			case 55:
+				//55 is our team special defined code to notify the shell to change workingDirectory
+				workingDirectory = new File(executionResult);
+				message = executionResult;
+				break;
+			case 98:
+				message = "Parsing Failed";
+				break;
+			case 126:
+				message = "Command invoked cannot execute";
+				break;
+			case 127:
+				message = "Command not found";
+				break;
+			case 210:
+				message = "A command was null";
+			default:
+				message = "Error Detected";
+				break;
+			}
+			return message;              	
+		}
+
+		
+        /**
+         * Run the execution tool and print the specific result to the shell
+         */
+		@Override
+		public void run() {
+			//check whether there is any tool to be executed here
+			if(executionTool != null){
+				final String executionResult = executionTool.execute(workingDirectory, standardIn);
+				if(executionResult != null){
+					String message = getMessage(executionTool.getStatusCode(), executionResult);
+					out.println(message);            
+				}
+			}
+			else{
+				out.println("There is no tool associated with the given command!");
+			}
+		}
+	}
+	
+    /**
+     * Getter to return this shell working directory
+     */
+	public File getWorkingDirectory(){
+		return workingDirectory;
+	}
+
 }
---
+++ test/sg/edu/nus/comp/cs4218/impl/PrivateTestingExecutingCommandThread.java
@@ -0,0 +1,66 @@
+package sg.edu.nus.comp.cs4218.impl;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import sg.edu.nus.comp.cs4218.ITool;
+import sg.edu.nus.comp.cs4218.impl.fileutils.PWDTool;
+import sg.edu.nus.comp.cs4218.impl.fileutils.CdTool;
+
+public class PrivateTestingExecutingCommandThread {
+
+	Shell shell;
+	ITool tool;
+	String stdin;
+	Shell.ExecutingCommandThread executingCommandThread;
+
+	@Before
+	public void before(){
+		shell = new Shell();
+		tool = new PWDTool(new String[]{"args"}); //we can use any other tool, but we use PWD for simplicity
+		stdin = null;
+		executingCommandThread = shell.new ExecutingCommandThread(tool, stdin);
+	}
+	
+	/**
+	 * Check whether this thread get a proper message given status code == 0
+	 */
+	@Test
+	public void testGetMessageWithSuccesfulStatusCode() {
+		int statusCode = 0;
+		String executionResult = tool.execute(shell.getWorkingDirectory(), stdin);
+		String message = executingCommandThread.getMessage(statusCode, executionResult);
+		assertTrue(message.equals(executionResult));
+	}
+	
+	/**
+	 * Check whether this thread fetch a proper result given status code != 0
+	 */
+	@Test
+	public void testGetMessageWithErrorStatusCode() {
+		int statusCode = 1;
+		String executionResult = tool.execute(shell.getWorkingDirectory(), stdin);
+		String message = executingCommandThread.getMessage(statusCode, executionResult);
+		assertFalse(message.equals(executionResult)); //assert that it doesn't print the execution result, i.e. it prints error message
+	}
+
+	/**
+	 * Check whether the current working directory is being changed when status code == 55
+	 */
+	@Test
+	public void testChangeDirectoryWhenStatusCodeIs55() {
+		int statusCode = 55;
+		String oldWorkingDir = shell.getWorkingDirectory().getAbsolutePath(); //get old working directory
+		File dummyDirectory = new File("dummyDirectory"); //create a dummy directory
+		dummyDirectory.mkdir();
+		tool = new CdTool(new String[]{"args", dummyDirectory.getAbsolutePath()}); //get the cd tool
+		String executionResult = tool.execute(shell.getWorkingDirectory(), stdin); //execute the cd tool
+		executingCommandThread.getMessage(statusCode, executionResult); //change the shell directory
+		String newDir = shell.getWorkingDirectory().getAbsolutePath(); //get new working directory
+		assertFalse(newDir.equals(oldWorkingDir)); //assert that the current working directory has been changed
+	}
+}
